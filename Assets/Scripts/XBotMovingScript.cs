6f1ed18525285c203e4597372cdb4067c71a33ee
helmholtz <helmholtz@fomal.host>
2022-11-04 16:26:38 +0800

    camera now doesn't shake with the character anymore

--- Assets/Scripts/XBotMovingScript.cs
+++ Assets/Scripts/XBotMovingScript.cs
@@ -5,17 +5,28 @@ using UnityEngine;
 [RequireComponent(typeof(Animator))]
 public class XBotMovingScript : MonoBehaviour
 {
+    [SerializeField] private float maximumForwardSpeed;
+    [SerializeField] private float maximumBackwardSpeed;
+    [SerializeField] private float mouseSensitivity;
+    [SerializeField] private bool invertY;
+    
     private Animator _animator;
+    private Transform _rootTransform;
     void Start()
     {
         // Getting current animator
         _animator = GetComponent<Animator>();
+        // Getting XBotRoot transform
+        _rootTransform = transform.parent.GetComponent<Transform>();
     }
 
     void Update()
     {
-        float axisV = Input.GetAxis("Vertical");
-        transform.position += Time.deltaTime * axisV * 5 * transform.forward;
-        _animator.SetFloat("movingSpeed", axisV * 5);
+        float axisVertical = Input.GetAxis("Vertical");
+        float axisHorizontal = Input.GetAxis("Horizontal");
+        float mouseX = Input.GetAxis("Mouse X");
+        float mouseY = Input.GetAxis("Mouse Y");
+        _rootTransform.position += Time.deltaTime * axisVertical * 5 * _rootTransform.forward;
+        _animator.SetFloat("movingSpeed", axisVertical * 5);
     }
 }
